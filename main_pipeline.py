7b5f274006e97d2cac7cf9eb9c4c0482a797e04c
Rynaa Grover <grover.rynaa14@gmail.com>
2024-08-02 13:13:39 -0700

    Update prompt

--- main_pipeline.py
+++ main_pipeline.py
@@ -34,11 +34,11 @@ def load_demo(folder, n_trajs,corrective = 'N'):
                 trajectory = pickle.load(open(path, 'rb'))
                 trajs.append(trajectory)
 
-        corrective_set = trajs[-15:]
+        corrective_set = trajs[-5:]
         
-        # sampled_set = np.random.choice(np.arange(len(trajs) - 5),size = 10,replace=False)
-        # for i in sampled_set:
-        #     corrective_set += [trajs[i]]
+        sampled_set = np.random.choice(np.arange(len(trajs) - 5),size = 10,replace=False)
+        for i in sampled_set:
+            corrective_set += [trajs[i]]
         
         return corrective_set
         
@@ -105,47 +105,62 @@ if __name__ == "__main__":
         pict = PIL.Image.open('results/shapley_2_'+str(rounds)+'_.png')
         prompt = """We are training an inverse reinforcement learning model using Deep Max Entropy algorithm on the Wumpus World environment. The model uses small feedforward neural networks to learn the reward function from the given demonstrations. The training manual of the game given below has information about the environment.
 
-        * TRAINING MANUAL * 
+
+        * TRAINING MANUAL *
+
 
         The game starts with the player at location [0,0] of the frozen lake grid world with the goal (treasure) located at far extent of the world e.g. [3,3] for the 4x4 environment.The player makes moves until they reach the goal or fall in a hole or gets eaten by the monster. There is always a path to the goal.
 
-        Holes in the ice are distributed in set locations of [(1,1), (1,3), (2,3), (3,0)]. The monster starts at location (2,2). It chooses uniformly randomly whether it wants to proceed in an upward direction [(2,2), 1,2), (0,2)] or towards the left [(2,2), (2,1), (2,0]. 
+
+        Holes in the ice are distributed in set locations of [(1,1), (1,3), (2,3), (3,0)]. The monster starts at location (2,2). It chooses uniformly randomly whether it wants to proceed in an upward direction [(2,2), 1,2), (0,2)] or towards the left [(2,2), (2,1), (2,0].
+
 
         ## Action Space
         The action shape is (1,) in the range {0, 3} indicating which direction to move the player.
-            - 0: Move left
-            - 1: Move down
-            - 2: Move right
-            - 3: Move up
+        - 0: Move left
+        - 1: Move down
+        - 2: Move right
+        - 3: Move up
 
-        The agent is in a stochastic environment where with probability 0.1 to move in a random direction and probability 
+
+        The agent is in a stochastic environment where with probability 0.1 to move in a random direction and probability
+
 
         ## Observation Space
         The observation is a value representing the player's current position as current_row * ncols + current_col (where both the row and col start at 0). For example, the goal position in the 4x4 map can be calculated as follows: 3 * 4 + 3 = 15. The number of possible observations is dependent on the size of the map.The observation is returned as an int().
 
+
         ## Starting State
         The episode starts with the player in state [0] (location [0, 0]).
 
+
         ## Episode End
         The episode ends if the following happens:
-                1. The player moves into a hole.
-                2. The player reaches the goal at max(nrow) * max(ncol) - 1 (location [max(nrow)-1, max(ncol)-1]).
-                3. The player gets eaten by the monster.
+        1. The player moves into a hole.
+        2. The player reaches the goal at max(nrow) * max(ncol) - 1 (location [max(nrow)-1, max(ncol)-1]).
+        3. The player gets eaten by the monster.
+
 
         * END OF TRAINING MANUAL *
 
-        In order to understand the model better we use SHAP. The SHAPley values for the above model are given in the plot below. These are the features - 
-        1. Distance left to reach the goal. 
-        2. Distance to the nearest hole. 
-        3. Location of the monster 
-        4. Distance to the monster 
-        5. Location of the agent 
-        
-        Additional features cannot be provided and you should not recommend providing more features. 
+
+        In order to understand the model better we use SHAP. The SHAPley values for the above model are given in the plot below. These are the features -
+        1. Distance left to reach the goal.
+        2. Distance to the nearest hole.
+        3. Location of the monster
+        4. Distance to the monster
+        5. Location of the agent
+        Additional features cannot be provided and you should not recommend providing more features.
+
+
+        It is very important to remember that negative distance encourages the agent to move closer to the object and positive distance encourages it to move away from the object.
+
 
         Can you explain these values?
         What features the robot is using to learn the reward function? Your response will help the demonstrator in understanding the robot learning better. How would you suggest these values should be modified to achieve the best reward function?
-        Be critical in your evaluation of how the demonstrator can improve the agent. Remember positive distance value encourages the agent to increase the distance and negative distance value encourages it to decrease the distance."""
+        Be critical in your evaluation of how the demonstrator can improve the agent.
+
+        """
 
         feature_shaps = pickle.load(open('/Users/rynaa/IRL_Project/new_results/shapley_values.pkl', 'rb'))
 
